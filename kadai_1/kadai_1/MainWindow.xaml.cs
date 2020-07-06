838d045a4707ace464b0065a92dde176718abc49
takashi_iizuka <takashi_iizuka@sobal.co.jp>
2020-07-06 16:15:38 +0900

    Test

---
+++ kadai_1/kadai_1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace kadai_1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WindowChild windowChild = new WindowChild();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            windowChild.Show();
+        }
+    }
+}
---
+++ kadai_1/kadai_1/RGBConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace kadai_1
+{
+    public class RGBConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return Color.FromRgb((byte)(double)(values[0]), (byte)(double)(values[1]), (byte)(double)(values[2]));
+            }
+            catch (InvalidCastException ic)
+            {
+                return Color.FromRgb((byte)(double)(0), (byte)(double)(0), (byte)(double)(0));
+          }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
